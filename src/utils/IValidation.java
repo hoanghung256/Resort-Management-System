fe7029ebb355af5ab34c8e74cfc42709400d438a
QuocAnh-dev <quocanh.dev404@gmail.com>
2024-01-24 09:33:18 +0700

    define regrex for validation and define all method from IValidation interface

--- src/utils/IValidation.java
+++ src/utils/IValidation.java
@@ -6,15 +6,15 @@ package utils;
  */
 public interface IValidation {
     // Define regex for employee ID format: EMP-YYYY (YYYY is digit from 0-9)
-    static final String EMPID_REGEX = " ";
+    static final String EMPID_REGEX = "^EMP-[0-9]{4}$";
     // Define regex for customer ID format: CUS-YYYY (YYYY is digit from 0-9)
-    static final String CUSID_REGEX = " ";
+    static final String CUSID_REGEX = "^CUS-[0-9]{4}$";
     // Define regex for person name that uppercase first character of each word 
-    static final String NAME_REGEX = " ";
+    static final String NAME_REGEX = "^[A-Z][a-z]*(\\\\s[A-Z][a-z]*)*$";
     // Define regex for citizen identification number have 12 digits
-    static final String IDENTIFICATION_REGEX = " ";
+    static final String IDENTIFICATION_REGEX = "^\\d{12}$";
     // Define regex for phone number have 10 digits and start with 0
-    static final String PHONE_REGEX = " ";
+    static final String PHONE_REGEX = "^0\\d{9}$";
     
     public String getString(String msg);
     
--- src/utils/Validation.java
+++ src/utils/Validation.java
@@ -4,6 +4,39 @@ package utils;
  *
  * @author hoang hung
  */
-public class Validation {
-    
+public class Validation implements IValidation{
+    @Override
+    public String getString(String msg) {
+        return null;
+    }
+
+    @Override
+    public String getAndValidEmpId(String msg) {
+        return null;
+    }
+
+    @Override
+    public String getAndValidCusId(String msg) {
+        return null;
+    }
+
+    @Override
+    public String getAndValidPersonName(String msg) {
+        return null;
+    }
+
+    @Override
+    public String getAndValidIdentificationNum(String msg) {
+        return null;
+    }
+
+    @Override
+    public double getAndValidSalary(String msg) {
+        return 0;
+    }
+
+    @Override
+    public String getAndValidPhone(String msg) {
+        return null;
+    }
 }
