a5cdd7cba5b47450a3b7a9be0b81de515f87379a
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-01-26 10:24:30 +0700

    Sync

--- src/utils/IValidation.java
+++ src/utils/IValidation.java
@@ -21,8 +21,8 @@ public interface IValidation {
     // Define regex for phone number have 10 digits and start with 0
     static final String PHONE_REGEX = "^0\\d{9}$";
     // Define date format for DATE
-    static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-    //use try catch to format date. Ex: LocalDate validDate = LocalDate.parse(DateStr, dateFormatter);
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    //use try catch to format date. Ex: LocalDate validDate = LocalDate.parse(DateStr, formatter)
     
     public String getString(String msg);
     
