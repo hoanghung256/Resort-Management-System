138623d2f5e16be2766635e36833db3dd0ff34a9
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-01-30 08:28:12 +0700

    Merge branch 'RM-15'

--- src/controller/FuramaController.java
+++ src/controller/FuramaController.java
@@ -1,6 +1,5 @@
 package controller;
 
-<<<<<<< HEAD
 import java.util.Date;
 
 import model.Employee;
@@ -8,9 +7,6 @@ import model.Employee;
 import java.time.LocalDate;
 
 import model.Customer;
-=======
-import model.Employee;
->>>>>>> RM-15
 import repository.CustomerRepository;
 import repository.EmployeeRepository;
 import repository.ICustomerRepository;
@@ -25,7 +21,8 @@ import view.Menu;
  */
 public class FuramaController extends Menu<String> {
     private static final String MENU_TITLE = "FURAMA RESORT MANAGEMENT";
-    private static final String[] MENU_OPTIONS = {"Employee Management", "Customer Management", "Facility Management", "Booking Management", "Promotion Management", "Exit"};
+    private static final String[] MENU_OPTIONS = { "Employee Management", "Customer Management", "Facility Management",
+            "Booking Management", "Promotion Management", "Exit" };
     private Menu<String> employeeManagementMenu;
     private Menu<String> customerManagementMenu;
     private Validation val;
@@ -34,7 +31,7 @@ public class FuramaController extends Menu<String> {
         super(MENU_TITLE, MENU_OPTIONS);
         val = new Validation();
     }
-    
+
     @Override
     public void execute(int choice) {
         switch (choice) {
@@ -56,46 +53,50 @@ public class FuramaController extends Menu<String> {
             case 6:
                 System.out.println("Goodbye!");
                 System.exit(0);
-                break;    
+                break;
         }
     }
-    
+
     private void runEmployeeManagementMenu() {
         String title = "EMPLOYEE MANAGEMENT";
-        String[] options = {"Display employees list", "Add new employee", "Edit employee information", "Return main menu"};
+        String[] options = { "Display employees list", "Add new employee", "Edit employee information",
+                "Return main menu" };
         IEmployeeRepository employeeRepo = new EmployeeRepository();
         EmployeeService employeeService = new EmployeeService(employeeRepo);
-        
+
         employeeManagementMenu = new Menu<String>(title, options) {
             @Override
             public void execute(int choice) {
-                Validation validation = new Validation();
                 switch (choice) {
                     case 1:
                         employeeService.display();
                         break;
                     case 2:
-                        String id = validation.getAndValidEmpId("Enter new employee ID: ");
-                        String name = validation.getAndValidPersonName("Enter new employee's Name: ");
-                        String gender = validation.getAndValidValue("Enter employee's gender: ", "M|F", "Invalid gender! Please input again!");
-                        boolean genderEmployee = gender.equals("M")? true : false;
-                        LocalDate date = validation.getAndValidDate("Enter employee's date of birth: ");
+                        String id = val.getAndValidEmpId("Enter new employee ID: ");
+                        String name = val.getAndValidPersonName("Enter new employee's Name: ");
+                        String gender = val.getAndValidValue("Enter employee's gender: ", "M|F",
+                                "Invalid gender! Please input again!");
+                        boolean genderEmployee = gender.equals("M") ? true : false;
+                        LocalDate date = val.getAndValidDate("Enter employee's date of birth: ");
                         Date dateOfBirth = java.sql.Date.valueOf(date);
-                        String identity = validation.getAndValidIdentificationNum("Enter employee's identification number: ");
-                        String phoneNumber = validation.getAndValidPhone("Enter employee's phone number: ");
-                        String email = validation.getAndValidValue("Enter employee's email: ", "^(.+)@(\\S+)$", "Invalid email. Please enter email again!");
-                        String level = validation.getString("Enter employee's level: ");
-                        String position = validation.getString("Enter employee's position: ");
-                        Double salary = validation.getAndValidSalary("Enter employee's salary: ");
-                        Employee e = new Employee(id, name, dateOfBirth, identity, genderEmployee, phoneNumber, email, level, position, salary);
-                        employeeService.add(e);
+                        String identity = val.getAndValidIdentificationNum("Enter employee's identification number: ");
+                        String phoneNumber = val.getAndValidPhone("Enter employee's phone number: ");
+                        String email = val.getAndValidValue("Enter employee's email: ", "^(.+)@(\\S+)$",
+                                "Invalid email. Please enter email again!");
+                        String level = val.getString("Enter employee's level: ");
+                        String position = val.getString("Enter employee's position: ");
+                        Double salary = val.getAndValidSalary("Enter employee's salary: ");
+                        Employee newEmp = new Employee(id, name, dateOfBirth, identity, genderEmployee, phoneNumber,
+                                email,
+                                level, position, salary);
+                        employeeService.add(newEmp);
                         break;
                     case 3:
                         // Use findById() to know which person user want to edit then call update()
                         Employee e;
                         do {
-                            String id = val.getAndValidEmpId("Enter employer ID: ");
-                            e = employeeService.findById(id);
+                            String inputId = val.getAndValidEmpId("Enter employer ID: ");
+                            e = employeeService.findById(inputId);
                         } while (e == null);
                         employeeService.update(e);
                         break;
@@ -105,13 +106,14 @@ public class FuramaController extends Menu<String> {
                 }
             }
         };
-        
+
         employeeManagementMenu.run();
     }
-    
+
     private void runCustomerManagementMenu() {
         String title = "CUSTOMER MANAGEMENT";
-        String[] options = {"Display customers list", "Add new customer", "Edit customer information", "Return main menu"};
+        String[] options = { "Display customers list", "Add new customer", "Edit customer information",
+                "Return main menu" };
         ICustomerRepository customerRepo = new CustomerRepository();
         CustomerService customerService = new CustomerService(customerRepo);
 
@@ -120,21 +122,25 @@ public class FuramaController extends Menu<String> {
             public void execute(int choice) {
                 switch (choice) {
                     case 1:
-                        
+
                         break;
                     case 2:
-                        String id = validation.getAndValidCusId("Enter customer ID: ");
-                        String fullName = validation.getAndValidPersonName("Enter customer's name: ");
-                        LocalDate date = validation.getAndValidDate("Enter date of birth: ");
+                        String id = val.getAndValidCusId("Enter customer ID: ");
+                        String fullName = val.getAndValidPersonName("Enter customer's name: ");
+                        LocalDate date = val.getAndValidDate("Enter date of birth: ");
                         Date dateOfBirth = java.sql.Date.valueOf(date);
-                        String identity = validation.getAndValidIdentificationNum("Enter Identification number: ");
-                        String gen = validation.getAndValidValue("Enter customer's gender (M/F): ", "[MF]", "Gender must be Male or Female!");
-                        boolean gender= gen.equals("M")? true:false;
-                        String phoneNumber = validation.getAndValidPhone("Enter phone number: ");
-                        String email = validation.getAndValidValue("Enter customer email: ", "(.+)@(\\S+)$", "Email must be follow YY..Y@gmail.com");
-                        String level = validation.getString("Enter Customer level: ");
-                        String address = validation.getAndValidValue("Enter Address: ", "^[A-Z][a-z]*(\\s[A-Z][a-z]*)*$", "Address must be upper case first character of each word!");
-                        Customer c = new Customer(id, fullName, dateOfBirth, identity, gender, phoneNumber, email, level, address);
+                        String identity = val.getAndValidIdentificationNum("Enter Identification number: ");
+                        String gen = val.getAndValidValue("Enter customer's gender (M/F): ", "[MF]",
+                                "Gender must be Male or Female!");
+                        boolean gender = gen.equals("M") ? true : false;
+                        String phoneNumber = val.getAndValidPhone("Enter phone number: ");
+                        String email = val.getAndValidValue("Enter customer email: ", "(.+)@(\\S+)$",
+                                "Email must be follow YY..Y@gmail.com");
+                        String level = val.getString("Enter Customer level: ");
+                        String address = val.getAndValidValue("Enter Address: ", "^[A-Z][a-z]*(\\s[A-Z][a-z]*)*$",
+                                "Address must be upper case first character of each word!");
+                        Customer c = new Customer(id, fullName, dateOfBirth, identity, gender, phoneNumber, email,
+                                level, address);
                         customerService.add(c);
                         break;
                     case 3:
@@ -142,11 +148,11 @@ public class FuramaController extends Menu<String> {
                         break;
                     case 4:
                         customerService.save();
-                        break;    
+                        break;
                 }
             }
         };
-        
+
         customerManagementMenu.run();
     }
 }
